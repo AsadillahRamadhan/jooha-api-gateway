c6152276b462b47faeede66867930d60b26e049c
AsadillahRamadhan <curutcepat@gmail.com>
2024-06-21 08:46:57 +0700

    update eslint

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT);
-  
 }
 bootstrap();
