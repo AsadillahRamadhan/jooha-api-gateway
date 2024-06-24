2faeaa38c92296c96e3ef170f745b26355f48c0f
AsadillahRamadhan <curutcepat@gmail.com>
2024-06-24 18:07:38 +0700

    add header validation

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -6,6 +6,7 @@ import { UserService } from 'src/services/user.service';
 export class AuthenticateMiddleware implements NestMiddleware {
   constructor(private readonly userService: UserService) {}
   use(req: Request, res: Response, next: NextFunction) {
+    if (!req.headers['authorization']) return res.sendStatus(401);
     const token = req.headers['authorization'].split(' ')[1];
     this.userService.verifyToken(token).subscribe((result: any) => {
       if (!result[0].isVerified) return res.sendStatus(401);
